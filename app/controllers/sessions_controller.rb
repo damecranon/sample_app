83434b91368e0746f604af56e8f0a6f3e98bb2dd
Damien Cranon <dame.cranon@gmail.com>
2012-07-09 14:32:37 -0700

    user micropost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,10 @@ class SessionsController < ApplicationController
       #Sign the user in and redirect to the user's show page.
       sign_in user
       redirect_to user
-    end
-  else
+    else
     flash[:error] = 'Invalid email/password combination' #Not quite right!
     render 'new'
+    end
   end
 
   def destroy
