25a92fa5ec8fec7e8300c50ed677e6b745913476
Damien Cranon <dame.cranon@gmail.com>
2012-07-10 14:04:10 -0700

    More edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
 
@@ -19,7 +19,9 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: {minimum: 6 }
   validates :password_confirmation, presence:true
 
